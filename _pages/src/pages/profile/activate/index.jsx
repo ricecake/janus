6e4d09ed41499ebec10a13d8d71a89ef96f92627
Sebastian Green-Husted <geoffcake@gmail.com>
2019-12-07 15:55:27 -0500

    theme and basic grid

--- _pages/src/pages/profile/activate/index.jsx
+++ _pages/src/pages/profile/activate/index.jsx
@@ -2,27 +2,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HelloWorld from 'Component/HelloWorld';
-import { ThemeProvider } from '@material-ui/core/';
+import { ThemeProvider, Grid } from '@material-ui/core/';
 import { createMuiTheme } from '@material-ui/core/styles';
 import green from '@material-ui/core/colors/green';
 import teal from '@material-ui/core/colors/teal';
 
 const theme = createMuiTheme({
-    palette: {
-      primary: green,
-      secondary: teal,
-    },
-    status: {
-      danger: 'orange',
-    },
+	palette: {
+	  primary: green,
+	  secondary: teal,
+	},
+	status: {
+	  danger: 'orange',
+	},
   });
 
 const root = document.getElementById('main');
 
-ReactDOM.render(
-  (
-    <ThemeProvider theme={theme}>
-        <HelloWorld />
-    </ThemeProvider>
-  ), root,
-);
+ReactDOM.render((
+	<ThemeProvider theme={theme}>
+		<Grid container direction="row" justify="center" alignItems="center">
+			<HelloWorld />
+		</Grid>
+	</ThemeProvider>
+), root);
