9584f9b6dd7ad015c1a003ec77d43e87ba5d582e
Sebastian Green-Husted <geoffcake@gmail.com>
2019-12-07 14:49:28 -0500

    update components import

--- _pages/webpack.config.js
+++ _pages/webpack.config.js
@@ -3,39 +3,42 @@ const path = require("path");
 const webpack = require("webpack");
 
 let files = glob.sync('./src/pages/**/index.jsx').reduce((acc, path) => {
-  const entry = path.replace('./src/', '').replace('/index.jsx', '')
-  acc[entry] = path
-  return acc
+	const entry = path.replace('./src/', '').replace('/index.jsx', '')
+	acc[entry] = path.replace('/src', '');
+	return acc
 }, {});
 
-console.log(files)
-
 module.exports = {
-  entry: files,
-
-output: {
-    filename: './build/[name].js',
-    path: path.resolve(__dirname)
-},
-optimization: {
-  minimize: true,
-  usedExports: true,
-},
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+	entry: files,
+	output: {
+		filename: './build/[name].js',
+		path: path.resolve(__dirname)
+	},
+	context: path.resolve(__dirname, 'src/'),
+	resolve: {
+		extensions: ["*", ".js", ".jsx"],
+		modules: [path.resolve('./node_modules')],
+		alias: {
+			Component: path.resolve(__dirname, 'src/components/')
+		}
+	},
+	optimization: {
+		minimize: true,
+		usedExports: true,
+	},
+	mode: "production",
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: "babel-loader",
+				options: { presets: ["@babel/env"] }
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+		]
+	},
 };
-
